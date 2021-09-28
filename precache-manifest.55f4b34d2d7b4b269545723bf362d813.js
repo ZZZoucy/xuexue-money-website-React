3ee244aa231ee68de2a3f6aced8687e0d82e4757
xiaozou <1424113865@qq.com>
2021-09-28 17:36:21 +0800

    deploy

---
+++ precache-manifest.55f4b34d2d7b4b269545723bf362d813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5463333cd3d3f0bbab28e8ae2a5f062",
+    "url": "/xuexue-money-website-React/index.html"
+  },
+  {
+    "revision": "9e07ad35b4753321fdfd",
+    "url": "/xuexue-money-website-React/static/css/main.27d68910.chunk.css"
+  },
+  {
+    "revision": "c39e49e73d07621b436f",
+    "url": "/xuexue-money-website-React/static/js/2.0bc465ce.chunk.js"
+  },
+  {
+    "revision": "fc8b115d4064d6797b6def218d457695",
+    "url": "/xuexue-money-website-React/static/js/2.0bc465ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e07ad35b4753321fdfd",
+    "url": "/xuexue-money-website-React/static/js/main.55ec11e3.chunk.js"
+  },
+  {
+    "revision": "0abc38dd8d2ddb08ada8",
+    "url": "/xuexue-money-website-React/static/js/runtime-main.cd1b61ee.js"
+  }
+]);
